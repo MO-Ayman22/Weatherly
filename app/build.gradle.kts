04ee9bc910bee2fad03c7fa1d0cec75d703190d4
Mohamed Ayman <2moo.ayman@gmail.com>
2026-03-08 15:31:38 +0200

    Refactor project structure and update dependencies
    - Move MainActivity and theme files into the presentation layer.
    - Update AndroidManifest and package declarations to match the new structure.
    - Upgrade Kotlin and Compose BOM versions.
    - Add Room, Retrofit, Gson, SplashScreen, MockK, and coroutine testing dependencies.
    - Enable KSP plugin for annotation processing.
    - Remove unnecessary device streaming configs from .idea.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.devtools.ksp)
 }
 
 android {
@@ -47,10 +48,21 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     testImplementation(libs.junit)
+    testImplementation(libs.mockk)
+    testImplementation(libs.kotlinx.coroutines.test)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation(libs.androidx.core.splashscreen)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.gson)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+    testImplementation(libs.androidx.room.testing)
+
 }
--- app/src/main/java/com/example/weatherly/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/weatherly/presentation/view/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.weatherly
+package com.example.weatherly.presentation.view
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.weatherly.ui.theme.WeatherlyTheme
+import com.example.weatherly.presentation.ui.theme.WeatherlyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
